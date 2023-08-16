a21004a90200a60b41c2b159dbd945b6927f0ecc
Tuskon <xgjlrs@gmail.com>
2023-08-15 23:06:26 -0400

    prototype

---
+++ resources/views/loginsc.blade.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Screen</title>
+</head>
+<body>
+
+<header> 
+
+<img>
+
+</header>
+
+
+<div > 
+
+<form>
+
+<input type="text" placeholder="User"><br><br>
+<input type="text" placeholder="Password"><br><br>
+<input type="submit" value="submit">
+</form>
+
+</div>
+
+    
+</body>
+</html>
