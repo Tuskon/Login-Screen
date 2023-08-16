15c6059e73c463cb0a28c57e3c183ca67d9353e5
José Luiz Rodrigues dos Santos <jose.santos@flexam.local>
2023-08-16 16:21:50 -0400

    Alterações

--- resources/views/loginsc.blade.php
+++ resources/views/loginsc.blade.php
@@ -3,24 +3,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Screen</title>
+    <link href="/css/login.css" rel="stylesheet" >
 </head>
 <body>
 
 <header> 
 
-<img>
+<img src="https://t3.ftcdn.net/jpg/06/02/30/12/240_F_602301246_H52FR3BCQqeti8emnT3eY7Jsnw4sjLqO.jpg">
 
 </header>
 
 
-<div > 
+<div id="section" > 
 
 <form>
 
-<input type="text" placeholder="User"><br><br>
-<input type="text" placeholder="Password"><br><br>
+<input id ="inputBar" type="text" placeholder="User"><br><br>
+<input id ="inputBar" type="text" placeholder="Password"><br><br>
 <input type="submit" value="submit">
 </form>
+
+<a href="https://www.sorryapp.com/error-pages/">Esqueceu sua senha ?</a>
 
 </div>
 
